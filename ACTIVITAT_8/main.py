5e494634874fedd42e20745f58605fc7fe60b79e
angel_ivanov <angelspsv@gmail.com>
2024-11-11 23:57:47 +0100

    metode get return item

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+#endpoint d'exemple GET amb missatge
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+#endpoint metode GET return item
+@app.get("/item/{id_item}")
+async def returnItem(id_item):
+    return {"id_item": id_item}
