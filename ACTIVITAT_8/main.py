956c645970fc4b9afb2593602db1918695ded16f
angel_ivanov <angelspsv@gmail.com>
2024-11-12 11:48:35 +0100

    metode get que retorna un objecte

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -13,3 +13,23 @@ async def root():
 @app.get("/item/{id_item}")
 async def returnItem(id_item):
     return {"id_item": id_item}
+
+
+#endpoint que simula l'acces a una bbdd i el retorn de dades d'un alumne
+table_names = [{0: "Lluis", "curs": "DAW2B"},
+               {1: "Angel", "curs": "DAW2A"},
+               {2: "Marta", "curs": "DAM2C"},
+               {3: "Joan", "curs": "ASIX2Z"}]
+#agafa el id de l'alumne des del navegador
+@app.get("/student/{id}")
+async def student_info(id):
+    #pas de Str a int
+    id_alumne = int(id)
+    #si l'id existeix mostra el objecte/diccionari alumne
+    if (id_alumne > 0) and (id_alumne <= len(table_names)):
+        return table_names[id_alumne]
+    #si no existeix llancem una excepcio
+    else:
+        raise HTTPException(status_code=404, detail="Alumne no trobat")
+
+
