f791b27be323df2c9b7211a11ac57957d5a1ccb2
angel_ivanov <angelspsv@gmail.com>
2024-11-25 18:50:59 +0100

    fet contenidor a docker, readme de la practica i fitxer de connexio a bbdd

---
+++ ACTIVITAT_10/README.md
@@ -0,0 +1,1 @@
+### Fitxer readme per l'Activitat 10
---
+++ ACTIVITAT_10/connection.py
@@ -0,0 +1,32 @@
+# aquest fitxer estableix la connexio amb la base de dades
+
+import psycopg2
+
+
+def connection_db():
+    conn = psycopg2.connect(
+        database="mydatabase",
+        user="admin",
+        password="admin",
+        host="localhost",
+        port="5432"
+    )
+
+    print("Connexió establerta correctament")
+    return conn
+
+
+'''
+#per veure si retorna un objecte
+conn = psycopg2.connect(
+    database="mydatabase",
+    user="admin",
+    password="admin",
+    host="localhost",
+    port="5432"
+)
+
+connection = conn.cursor()
+
+print(connection)
+'''
