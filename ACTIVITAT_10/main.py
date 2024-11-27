833fde5fbf47c7b8abe65a88acd485e56c34e89f
angel_ivanov <angelspsv@gmail.com>
2024-11-27 20:50:22 +0100

    feina feta fins el final de classe

--- ACTIVITAT_10/main.py
+++ ACTIVITAT_10/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from connection import *
 
 app = FastAPI()
+
+
+class Tematica(BaseModel):
+    word: str
+    theme: str
 
 
 #endpoint d'exemple GET amb missatge
 @app.get("/")
 async def root():
     return {"message": "Hola!"}
+
+
+@app.get("/penjat/tematica/opcions", response_model=list[Tematica])
+async def tematica_opcions():
+    #codi per obtenir les tematiques
+    return
 
 
 
