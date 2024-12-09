c932075889a0d44ddfc8d53cde970b19623bee23
angel_ivanov <angelspsv@gmail.com>
2024-12-09 20:45:47 +0100

    endpoint comencar partida

--- ACTIVITAT_11/main.py
+++ ACTIVITAT_11/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from read_cadena import *
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +8,20 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hola!"}
+
+
+class Cadena(BaseModel):
+    id_cadena: int
+    cadena: str
+
+
+@app.get("/start_game/{id_cadena}")
+async def comencar_partida(id_cadena):
+    cadena = read_cadena_id(id_cadena)
+
+    if cadena is None:
+        raise HTTPException(status_code=404, detail="Cadena not found")
+
+    #retorna json
+    return {"id_cadena": cadena[0], "cadena": cadena[1]}
+
---
+++ ACTIVITAT_11/read_cadena.py
@@ -0,0 +1,23 @@
+from connection import *
+
+def read_cadena_id(id_cadena):
+    try:
+        conn = connection_db()
+        cur = conn.cursor()
+
+        #fem la consulta sql per veure si existeix el id_cadena
+        cur.execute("SELECT * FROM cadenas WHERE id_cadena = %s", (id_cadena,))
+        cadena = cur.fetchone()
+
+    #en cas d'error: no existeix l'id_cadena
+    except Exception as e:
+        print(f"Error al verificar el id_cadena: {e}")
+        return None
+
+    #si o si tanquem la connexio a la bbdd al final
+    finally:
+        cur.close()
+        conn.close()
+
+    # retorna True o False de la consulta sql
+    return cadena
