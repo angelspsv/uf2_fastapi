43033b1c3db276027508fe4932fe735411d46c0a
angel_ivanov <angelspsv@gmail.com>
2024-12-10 12:36:43 +0100

    endpoint abecedari fet

--- ACTIVITAT_11/main.py
+++ ACTIVITAT_11/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from read_cadena import *
 from pydantic import BaseModel
+from connection import *
 
 app = FastAPI()
 
@@ -14,7 +15,7 @@ class Cadena(BaseModel):
     id_cadena: int
     cadena: str
 
-
+#endpoint GET per retornar text començar partida
 @app.get("/start_game/{id_cadena}")
 async def comencar_partida(id_cadena):
     cadena = read_cadena_id(id_cadena)
@@ -25,3 +26,30 @@ async def comencar_partida(id_cadena):
     #retorna json
     return {"id_cadena": cadena[0], "cadena": cadena[1]}
 
+
+#endpoint per retornar l'abecedari
+@app.get("/abecedari/{id}")
+async def abecedari(id):
+    try:
+        conn = connection_db()
+        cur = conn.cursor()
+
+        #fem la consulta sql per obtenir l'abecedari que volem des de l'id desitjat
+        cur.execute("SELECT * FROM abecedaris WHERE id_abecedari = %s", (id,))
+        sql_abecedari = cur.fetchone()
+
+        #error de ID no trobat
+        if sql_abecedari is None:
+            raise HTTPException(status_code=404, detail="ID no trobat")
+
+    #error de tipus d'argument
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Error de base de datos: {str(e)}")
+
+    # si o si tanquem la connexio a la bbdd al final
+    finally:
+        cur.close()
+        conn.close()
+
+    #return format json
+    return {"id_abecedari": sql_abecedari[0], "nom_abecedari": sql_abecedari[1], "abecedari": sql_abecedari[2]}
